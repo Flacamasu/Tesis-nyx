fb814851b6f7dc10e738bc716711723e2cf8063e
Luis Araya <luis.27062000@gmail.com>
2023-09-03 22:37:15 -0300

    Actualizacion empresas

--- Server/server.js
+++ Server/server.js
@@ -20,12 +20,57 @@ const db = mysql.createPool({
   database: 'nyx',
 });
 
-app.get('/api/data', (req, res) => {
+app.get('/api/empresas', (req, res) => {
   db.query('SELECT * FROM empresas', (err, result) => {
     if (err) {
       console.log(err);
     } else {
       res.send(result);
+    }
+  });
+});
+
+app.get('/api/postulaciones', (req, res) => {
+  db.query('SELECT * FROM postulaciones', (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.get('/api/departamentos', (req, res) => {
+  db.query('SELECT * FROM departamentos', (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.post('/api/autenticar', (req, res) => {
+  const { usuario, contraseña } = req.body;
+
+  // Consulta SQL para buscar el usuario en la base de datos
+  const sql = 'SELECT * FROM empresas WHERE usuario = ? AND contraseña = ?';
+  
+  db.query(sql, [usuario, contraseña], (err, result) => {
+    console.log(usuario);
+    if (err) {
+      console.log(err);
+      res.status(500).json({ error: 'Error interno del servidor' });
+      return;
+    }
+
+    if (result.length > 0) {
+      // Autenticación exitosa
+      res.json({ autenticado: true });
+    } else {
+      // Autenticación fallida
+  
+      res.json({ autenticado: false });
     }
   });
 });
